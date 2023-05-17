d53619f4218f75026ad849d05563d635996f01af
ManausK <1905246@kiit.ac.in>
2023-05-17 20:33:52 +0530

    React Stopwatch Basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(null);
+  const [now, setNow] = useState(null);
+
+  const intervalRef = useRef(null);
+
+  const handleStart = () => {
+    setTime(Date.now());
+    setNow(Date.now());
+    intervalRef.current = setInterval(() => {
+      setTime(Date.now());
+    }, 10);
+  };
+
+  const handleStop = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  let timePassed = (time - now) / 1000;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        Stopwatch <br />
+        {timePassed.toFixed(3)}
+      </h2>
+      <div>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+      </div>
     </div>
   );
 }
